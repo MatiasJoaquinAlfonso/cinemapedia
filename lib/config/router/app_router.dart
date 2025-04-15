934966643edb08c353066bd579197b98a1950803
Alfonso Matias Joaquin <joaquinmatias332@gmail.com>
2025-04-15 00:29:03 -0300

    Aplicando cambios dentro de mi CustoimButtomAppBar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -2,12 +2,16 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) {
+        final String pageIndex = state.pathParameters['page'] ?? '0';
+        
+        return HomeScreen(pageIndex: int.parse(pageIndex));
+      },
       routes: [
         GoRoute(
           path: 'movie/:id',
@@ -17,8 +21,8 @@ final appRouter = GoRouter(
 
             return MovieScreen(movieId: movieId);
           },
-        ),  
-      ]
+        ),
+      ],
     ),
   ],
 );
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,18 +2,30 @@ import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
-
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key, 
+    required this.pageIndex
+  });
+
+  final viewRoutes = const <Widget>[
+    HomeView(), 
+    SizedBox(), // <--- categorias View
+    FavoritesView()
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes
+      ),
+
+      bottomNavigationBar: CustomBottomNavigation( currentIndex: pageIndex ),
     );
   }
 }
-
