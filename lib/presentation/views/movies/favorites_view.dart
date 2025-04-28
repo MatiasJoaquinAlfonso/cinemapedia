7c8202c97e9c11b0345249841941eec77f10ed47
Alfonso Matias Joaquin <joaquinmatias332@gmail.com>
2025-04-28 11:13:15 -0300

    Implementamos la pantalla de favoritos
    Traemos la informacion desde la base de datos para mostrarla en la pantalla de favoritos. Y continuamos con el diseño de esta pantalla

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,15 +1,39 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+//init
+//solo las primeras 10
+
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoritesMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Favorites view'),
+      body: ListView.builder(
+        itemCount: favoritesMovies.length,
+        itemBuilder: (context, index) {
+
+          final movie = favoritesMovies[index];
+
+          return ListTile(title: Text( movie.title ));
+        },
       ),
-      body: const Center(child: Text('Favoritos')),
     );
   }
 }
